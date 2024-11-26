4fb9aa4fc8c4fd96e7662be6058a0500c009f6d5
Prakash <prakash@pestrust.eu.in>
2024-11-26 13:54:41 +0530

    main in place

--- queue.c
+++ queue.c
@@ -1,1 +1,7 @@
-// Here goes queue data structure logic
+// Here goes queue data structure logic 
+int main(int argc, char const *argv[])
+{
+    /* code */
+    printf("this que is dynamic");
+    return 0;
+}
