338e9034f1ad51eaf46db6c8e074fca8d62af19c
Pushkar <=>
2024-12-02 14:27:24 +0530

    Stack Safe

--- queue.c
+++ queue.c
@@ -3,17 +3,18 @@
 int main(int argc, char const *argv[])
 {
     /* code */
-    printf("\nthis que is dynamic\n");
+    printf("\nthis queue is dynamic\n");
     insert();
+    display();
     return 0;
 }
 
 void insert()
 {
-    printf("here we will type insert logic later");
+    //printf("here we will type insert logic later");
 }
 
 void display()
 {
-    printf("here we will put display logic");
+    //printf("here we will put display logic");
 }
