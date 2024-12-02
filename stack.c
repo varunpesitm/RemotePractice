68b37f72d12525cab1422742227e1227f40ccdc6
=Ankitha <chinmaysr01gmail.com>
2024-12-02 15:06:16 +0530

    updates done be prachi to stack.c

--- stack.c
+++ stack.c
@@ -2,12 +2,14 @@
 int main(int argc, char const *argv[])
 {
     /* code */
+    printf("here i will call push function");
+    int i=push();
     return 0;
 }
 
 int push()
 {
-
+    printf("push function called");
 }
 
 int pop()
