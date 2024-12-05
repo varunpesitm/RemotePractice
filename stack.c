af8794e36c2ff108117425888cc3833da935f63f
Prakash <prakash@pestrust.eu.in>
2024-12-05 14:29:12 +0530

    Update: stack.c on 5 Dec 2024

--- stack.c
+++ stack.c
@@ -1,15 +1,21 @@
 // Here goes stack data structure logic
+#include <stdio.h>
+int stack[10], top=-1;
+void push();
 int main(int argc, char const *argv[])
 {
     /* code */
-    printf("here i will call push function");
-    int i=push();
+    printf("here i will call push function\n");
+    push();
     return 0;
 }
 
-int push()
+void push()
 {
-    printf("push function called");
+    int item;
+    printf("enter the element to push\n");
+    scanf("%d",&item);
+    stack[++top]=item;
 }
 
 int pop()
